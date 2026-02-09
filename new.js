0b6bef9a5c860c54a8d2e9b89bb9dafa5a277faf
mahtab-217 <mahtabnasiri120089@gmail.com>
2026-02-09 20:46:18 +0430

    Objects

--- new.js
+++ new.js
@@ -1,7 +1,15 @@
-const number=[1,2,3,4,5];
-number.push(6,7);
-number.pop();
+// const number=[1,2,3,4,5];
+// number.push(6,7);
+// number.pop();
 
-const num=[70,80,90];
-num.join(50);
-num.rev
+// const num=[70,80,90];
+// num.filter(0,23);
+// const pushed=num.unpush("9090");
+const studentList={
+    name: "Ali",
+    lastName: "Alizada",
+    age: 24
+}
+studentList.name="Ali Reza";
+studentList["score"]=90;
+delete studentList.lastName;
