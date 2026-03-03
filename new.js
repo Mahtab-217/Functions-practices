b7779a022346850cefd3194a8c84c24184b967ba
mahtab-217 <mahtabnasiri120089@gmail.com>
2026-03-03 20:47:26 +0430

    new project started

--- new.js
+++ new.js
@@ -1,14 +1,39 @@
-const section= document.querySelector("section");
-section.style.cssText="border: 1px solid black; border-reduice:9px; height:300px; width:80%; margin:0 auto";
-const h1=document.createElement("h1");
- section.append(h1);
- h1.textContent="In the name of allah";
-// h1.setAttribute("class","a")
-h1.classList.add("a");
-const p=document.createElement("p");
-p.innerHTML="Hello good Morning";
-section.appendChild(p);
-p.setAttribute("class","b");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const section= document.querySelector("section");
+// section.style.cssText="border: 1px solid black; border-reduice:9px; height:300px; width:80%; margin:0 auto";
+// const h1=document.createElement("h1");
+//  section.append(h1);
+//  h1.textContent="In The Name Of Allah";
+// //  h1.style.cssText=""
+// // h1.setAttribute("class","a")
+// h1.classList.add("a");
+// const p=document.createElement("p");
+// p.innerHTML="Hello Good Morning";
+// section.appendChild(p);
+// p.setAttribute("class","b");
+
+
 
 
 // const pList=document.querySelector("div > p");
